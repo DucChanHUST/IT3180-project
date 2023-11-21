6b50d1a5765b666aed56d539a1a043c9ec3f8704
duonganh <duonganh2973@gmail.com>
2023-11-21 10:36:00 +0700

    add test env to not check user role

--- server/util/checkUserRole.js
+++ server/util/checkUserRole.js
@@ -1,5 +1,7 @@
 function checkUserRole(allowedRoles) {
   return (req, res, next) => {
+    if (process.env.TEST == 1) return next()
+
     if (!req.decodedToken || !allowedRoles.includes(req.decodedToken.role)) {
       return res.status(403).json({ error: 'Permission denied' });
     }
