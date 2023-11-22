a2b72f30a0f26ac348abf9a6d79127d9b7923547
thaovann <lethithaovan2711@gmail.com>
2023-11-22 13:34:52 +0700

    registration update

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import configureStore from "./redux/store"
+import configureStore from "./redux/store";
+
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/lib/integration/react";
 
 ReactDOM.render(
-  <Provider store={configureStore}>
-    <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </React.StrictMode>
+    </PersistGate>
   </Provider>,
   document.getElementById("root"),
 );
-
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,35 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import userReducer from "./userSlice";
 import registrationReducer from "./registrationSlice";
 import residentReducer from "./residentSlice";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-export default configureStore({
-  reducer: {
-    auth: authReducer,
-    users: userReducer,
-    registration: registrationReducer,
-    resident: residentReducer,
-  },
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  users: userReducer,
+  registration: registrationReducer,
+  resident: residentReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
